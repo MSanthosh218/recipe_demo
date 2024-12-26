15847c82e8bebd653f05c47e8fa01eec571df0ca
M.Santosh <santhoshmsanthosh218@gmail.com>
2024-12-26 18:08:06 +0530

    responsive change

--- src/components/DetailsOfrecipe.jsx
+++ src/components/DetailsOfrecipe.jsx
@@ -29,11 +29,11 @@ const DetailsOfrecipe = () => {
 
     return (
         <div className="flex items-center justify-center flex-col">
-            <img src={recipe.strMealThumb} alt={recipe.strMeal} className="recipe-image" />
+            <img src={recipe.strMealThumb} alt={recipe.strMeal} className="w-full sm:w-72 md:w-80 lg:w-80 xl:w-96 max-sm:w-60" />
             <h2 className="recipe-title">{recipe.strMeal}</h2>
             <p><strong>Area:</strong> {recipe.strArea}</p>
             <p><strong>Main Ingredient:</strong> {recipe.strCategory}</p>
-            <h3>Instructions:</h3>
+            <h3> <strong>Instructions:</strong></h3>
             <p>{recipe.strInstructions}</p>
             
         </div>
